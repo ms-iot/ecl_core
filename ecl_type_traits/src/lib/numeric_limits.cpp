29bba8f8fc56d54c2b40aa20b9857fe2299e2551
James Xu <kejxu@microsoft.com>
2019-07-30 11:45:56 -0700

    add back lib subfolder for proper linking

---
+++ ecl_type_traits/src/lib/numeric_limits.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file /ecl_type_traits/src/lib/numeric_limits.cpp
+ *
+ * @brief Implementation of the extended numeric limit specialisations.
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <limits>
+#include "../../include/ecl/type_traits/numeric_limits.hpp"
+
+// this file is empty because ecl_type_traits is header-only
